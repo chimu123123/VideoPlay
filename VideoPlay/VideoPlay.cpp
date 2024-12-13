913ecc757df4bdbf88c03b3dfe87b35535cab903
zhj <1934316343@qq.com>
2024-12-13 22:00:15 +0800

    vlc小demo，关于一些基本函数的使用

--- VideoPlay/VideoPlay.cpp
+++ VideoPlay/VideoPlay.cpp
@@ -3,8 +3,49 @@
 
 #include <iostream>
 #include "vlc.h"
+#include <Windows.h>
+
+std::string Unicode2Utf8(const std::wstring& str)
+{
+	std::string strinfo;
+	
+	int lenght = ::WideCharToMultiByte(CP_UTF8, 0, str.c_str(), str.size(), NULL, 0, NULL, NULL);
+	strinfo.resize(lenght);
+	::WideCharToMultiByte(CP_UTF8, 0, str.c_str(), str.size(), (LPSTR)strinfo.c_str(), strinfo.size(), NULL, NULL);
+	return strinfo;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int argc = 1;
+	char* argv[2];
+	argv[0] = (char*)"--ignore-config";
+	libvlc_instance_t* vlc_ins=libvlc_new(argc,argv);
+	std::string path = Unicode2Utf8(L"C:\\Users\\19343\\Desktop\\西游记.mp4");
+	libvlc_media_t* media = libvlc_media_new_path(vlc_ins,path.c_str());
+	//media = libvlc_media_new_location();
+	libvlc_media_player_t* player = libvlc_media_player_new_from_media(media);
+	do
+	{
+		int ret = libvlc_media_player_play(player);
+		if (ret == -1)
+		{
+			printf("error found!\r\n");
+		}
+		Sleep(300);
+		libvlc_time_t tm=libvlc_media_player_get_length(player);
+		printf("%02d:%02d:%02d\r\n", (int)tm / 3600000, (int)(tm / 60000) % 60, (int)(tm/1000) % 60);
+		int width = libvlc_video_get_width(player);
+		int height = libvlc_video_get_height(player);
+		printf("width=%d height=%d\r\n", width, height);
+		getchar();
+		libvlc_media_player_pause(player);
+		getchar();
+		libvlc_media_player_play(player);
+		getchar();
+		libvlc_media_player_stop(player);
+	} while (0);
+	libvlc_media_player_release(player);
+	libvlc_media_release(media);
+	libvlc_release(vlc_ins);
 }
